46221b806d7e1c74ec158113b8948caabd0a0050
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-16 17:39:30 -0500

    started vpc stack

--- vpc1/lib/vpc1-stack.ts
+++ vpc1/lib/vpc1-stack.ts
@@ -1,10 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import autoscaling = require('aws-cdk-lib/aws-autoscaling');
+import ec2 = require('aws-cdk-lib/aws-ec2');
+import elbv2 = require('aws-cdk-lib/aws-elasticloadbalancingv2');
 import { Construct } from 'constructs';
 
 export class Vpc1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    const vpc = new ec2.Vpc(this, '2b-vpc-1')
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, '2b-alb-1', {
+      vpc,
+      internetFacing: true,
+    })
+
+
   }
 }
+
+const app = new cdk.App()
+new Vpc1Stack(app, 'vpc-1-stack')
+app.synth()
