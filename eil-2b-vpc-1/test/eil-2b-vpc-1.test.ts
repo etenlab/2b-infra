fa4f694fd6d62e9b96320a1307d0b88fdf8c1948
Michael Marshall <michaelmarshall220@gmail.com>
2022-08-16 14:28:33 -0500

    started first 2b infra vpc cdk project

---
+++ eil-2b-vpc-1/test/eil-2b-vpc-1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Eil2BVpc1 from '../lib/eil-2b-vpc-1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eil-2b-vpc-1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Eil2BVpc1.Eil2BVpc1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
